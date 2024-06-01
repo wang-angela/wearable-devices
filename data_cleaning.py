40b0e913d20d3d8a2f1a41fcd721940b1d1f3a01
Angela Wang <angelawang.aw@outlook.com>
2024-05-31 22:18:42 -0400

    Create heatmap function to save figures as png's

---
+++ data_cleaning.py
@@ -0,0 +1,4 @@
+
+# Removes participants who withdrew consent
+def withdrew_consent(target_df, consent_df):
+
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from IPython.display import display
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import data_cleaning as dc
 
-def read_data(filename):
+
+def read_data(filename, sheetname):
     # Prepare to parse Excel sheets into DataFrame
     xls = pd.ExcelFile('./data/' + filename)
+    # Read data into DataFrame from appropriate sheet
+    df = pd.read_excel(xls, sheetname)
+    # Drop column 0 because it reads in the labels
+    df.drop(df.columns[0], axis=1, inplace=True)
 
-    # Reading total sleep record sheet into DataFrame
-    totalsleep_df = pd.read_excel(xls, 'TotalSleepRecords')
+    display(df)
+    return df
+
+
+def create_heatmap(df, filename):
+    # Alter figsize to be larger
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # Draw a heatmap with the numeric values in each cell
+    sns.heatmap(df, fmt="d", linewidths=.5, ax=ax)
+    # Save heatmap
+    plt.savefig("./images/" + filename, dpi=600)
+
+    plt.show()
+
 
 if __name__ == '__main__':
-    read_data('SP24_rawSleepUpdated.xlsx')
+    # Reading in files
+    total_sleep_df = read_data('SP24_rawSleepUpdated.xlsx', 'TotalSleepRecords')
+    consent_info_df = read_data('SP24_consentInfo.xlsx', 'Sheet1')
 
+    # Cleaning Data
+    dc.withdrew_consent(total_sleep_df, consent_info_df)
+
+    create_heatmap(total_sleep_df, "Participation")
